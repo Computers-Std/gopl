505630bfc36f80e99a33027dbb4ec82015e69bc4
ukiran03 <ushakiranreddi@gmail.com>
2025-12-06 05:49:13 +0530

    revise: completed upto 8.3-Concurrent Echo server
    starting: 8.4 Channels

---
+++ _revise/ch8/netcat3/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	done := make(chan struct{})
+	go func() {
+		// if _, err := io.Copy(os.Stdout, conn); err != nil {
+		// 	log.Print(err) // reading from closed connection
+		// }
+
+		io.Copy(os.Stdout, conn) // NOTE ignoring errors
+		log.Println("done")
+
+		done <- struct{}{} // signal the main goroutine
+	}()
+
+	mustCopy(conn, os.Stdin) // main
+	conn.Close()
+	<-done // wait for background goroutine to finish
+}
+
+func mustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
